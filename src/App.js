2a273230f621a570266abc6d37db8cf005b6e5ea
Sathya-735 <sathyasiva735@gmail.com>
2025-04-05 11:26:51 +0530

    day8tasktotallycompleleted

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 function App() {
+  const [arr, setArr] = useState(["task", "task", "task"]);
+
+  const add = () => {
+    setArr([...arr, `task`]);
+    
+  };
+
+  const remove = (indexToRemove) => {
+    setArr(arr.filter((_, index) => index !== indexToRemove));
+  };
 
   return (
-    <div>
-<h1>hiii</h1>
+    <div style={{ textAlign: 'center' }}>
+      <h1>Day 8 Task</h1>
+      <ul style={{listStyleType:"none"}}>
+        {arr.map((task, index) => (
+          <li key={index}>
+            {task}
+            <button onClick={() => remove(index)}>x</button>
+          </li>
+        ))}
+      </ul>
+      <button onClick={add}>add</button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
